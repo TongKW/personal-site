990fd1f47dc26adbadf4fe8792c1594a6822feaf
Peter Tong <66624654+TongKW@users.noreply.github.com>
2024-01-06 00:47:19 +0800

    fix(git): delete app/Work directory

--- app/Work/chart/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function WorkChart() {
-  return (
-    <div className="flex flex-col w-full">
-      <h1 className="text-4xl font-bold my-6">{`Chart (Coming soon)`}</h1>
-    </div>
-  );
-}
--- app/Work/system/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function WorkSystem() {
-  return (
-    <div className="flex flex-col w-full">
-      <h1 className="text-4xl font-bold my-6">{`System (Coming soon)`}</h1>
-    </div>
-  );
-}
